d1a1da6587851963bddaabd39cbd7774647c28fd
qilan2 <123293369+qilan2@users.noreply.github.com>
2025-06-10 22:39:43 +0800

    Create config.default.js

---
+++ config.default.js
@@ -0,0 +1,8 @@
+//src/config.default.js，添加允许访问的IP白名单
+// 内部API的IP白名单配置
+export const internalApiWhitelist = [
+    '193.112.131.225',      // localhost
+    '47.115.51.63'
+    // '::1',            // localhost IPv6
+    // '192.168.1.0/24'  // 本地网络示例
+]; 
